85228a9ea3fadbb7c5b9663ed087aa0fda6cd933
Arya N M <aryasathy999@gmail.com>
2025-12-01 12:09:00 +0530

    Delete students_fd_bd directory

--- students_fd_bd/server/routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import {create,getAll} from '../controller/userController.js'
-const route=express.Router();
-route.post("/students",create);
-route.get("/students",getAll);
-export default route;
