56b8c96422a5e3bf1a72ff0bf229fb804c86acf7
Arya N M <aryasathy999@gmail.com>
2025-12-01 12:10:32 +0530

    Delete nodejs_tasks directory

--- nodejs_tasks/importingarrayofpalindromes.js
+++
@@ -1,4 +0,0 @@
-import palindromes, {getPalindromeCount} from "./exportingarrayofpalindromes.js";
-
-console.log("Palindrome words:",palindromes);
-console.log("Palindrome count:",getPalindromeCount());
