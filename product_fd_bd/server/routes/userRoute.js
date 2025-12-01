f9601bc9fbe5bcedc2cd58be9453ede5793a0b2b
Arya N M <aryasathy999@gmail.com>
2025-12-01 12:10:06 +0530

    Delete product_fd_bd directory

--- product_fd_bd/server/routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import {create,getAll} from '../controller/userController.js'
-const route=express.Router();
-route.post("/products",create);
-route.get("/products",getAll);
-export default route;
